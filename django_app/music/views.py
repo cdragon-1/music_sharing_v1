d43870a3801961cb7da235a07643d47c451700c7
cdragon-1 <streetfood89@gmail.com>
2017-03-30 01:58:21 +0900

    views에서 필요없는 내용 제거, search템플릿 수정

--- django_app/music/views.py
+++ django_app/music/views.py
@@ -5,38 +5,29 @@ import requests
 from django.shortcuts import render, redirect
 
 from music_share.settings import config
-from utils.settings import get_setting
-from music.models import Music
 
 
 def search_from_deezer(keyword, page_token=None):
     deezer_api_key = config['django']['secret_key']
-    # 3. requests 라이브러리를 이용(pip install requests), GET요청으로 데이터를 받아온 후
-    # 이렇게 Parameter와 URL을 분리합니다
     params = {
         'q': keyword,
-        # 'maxResults': 15,
+        'maxResults': 20,
         'type': 'artist',
         'key': deezer_api_key,
     }
-    # 페이지 토큰값이 전달되었을 때만 params에 해당 내용을 추가해서 요청
+
     if page_token:
         params['pageToken'] = page_token
 
     r = requests.get('https://api.deezer.com/2.0/search?q=', params=params)
-    pprint('r:{}'.format(r))
     result = r.text
 
-    # 4. 해당 내용을 다시 json.loads()를 이용해 파이썬 객체로 변환
+    # 해당 내용을 다시 json.loads()를 이용해 파이썬 객체로 변환
     result_dict = json.loads(result)
-    # print('result_dict:{}'.format(result_dict))
     return result_dict
 
 
 def search(request):
-    # print(request.path_info)
-    # print(request.get_full_path())
-
     musics = []
     context = {
         'musics': musics,
@@ -64,25 +55,14 @@ def search(request):
         pprint(items)
         for item in items:
             # 실제로 사용할 데이터
-            deezer_id = item['id']
             title = item['title']
-            # description = item['snippet']['description']
-            # published_date = parse(published_date_str)
-            # url_thumbnail = item['snippet']['thumbnails']['high']['url']
-            # 이미 북마크에 추가된 영상인지 판단
-            # is_exist = BookmarkVideo.objects.filter(
-            #     user=request.user,
-            #     video__youtube_id=youtube_id
-            # ).exists()
+            preview = item['preview']
+            picture = item['album']['cover_small']
 
-            # 현재 item을 dict로 정리
             cur_item_dict = {
                 'title': title,
-                # 'description': description,
-                # 'published_date': published_date,
-                # 'youtube_id': youtube_id,
-                # 'url_thumbnail': url_thumbnail,
-                # 'is_exist': is_exist,
+                'preview': preview,
+                'picture': picture,
             }
             musics.append(cur_item_dict)
 
